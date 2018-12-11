b64ce78c42a29278112e0e01233a907bccda6ad2
Yegor Stephanovich <stefanovich_egor@mail.ru>
2018-12-12 00:07:29 +0300

    Add constructor for generator

--- DependecyInjectionLibrary/DepecndencyGenerator.cs
+++ DependecyInjectionLibrary/DepecndencyGenerator.cs
@@ -8,5 +8,20 @@ namespace DependecyInjectionLibrary
 {
      public class DepecndencyGenerator
      {
+          private Validator validator;
+          private List<Dependency> dependecies;
+          private Dictionary<KeyValuePair<Type, Type>, object> singltonList;
+          
+
+          public DepecndencyGenerator(Configuration config)
+          {
+               singltonList = new Dictionary<KeyValuePair<Type, Type>, object>();
+               dependecies = new List<Dependency>(config.Dependencies);
+               validator = new Validator();
+
+               if (!validator.Validate(config))
+                    throw new ArgumentException("Invalid argument");
+
+          }
      }
 }
