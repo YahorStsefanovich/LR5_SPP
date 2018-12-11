51ebff7baaf37454cc50d231edb5d562bc8e3592
Yegor Stephanovich <stefanovich_egor@mail.ru>
2018-12-11 22:53:25 +0300

    Change method Validate

--- DependecyInjectionLibrary/Validator.cs
+++ DependecyInjectionLibrary/Validator.cs
@@ -8,5 +8,29 @@ namespace DependecyInjectionLibrary
 {
      public class Validator
      {
+          private IEnumerable<Dependency> dependecies;
+
+          public bool Validate(Configuration config)
+          {
+               dependecies = config.Dependencies;
+               foreach (Dependency dependency in dependecies)
+               {
+                    bool result = false;
+                    if (dependency.pair.Key.IsGenericTypeDefinition &&
+                         !dependency.pair.Value.IsGenericTypeDefinition)
+                    {
+                         return result;
+                    }
+
+                    if (dependency.pair.Key != dependency.pair.Value &&
+                         !dependency.pair.Key.IsAssignableFrom(dependency.pair.Value))
+                         return result;
+
+                    if (dependency.pair.Value.IsAbstract)
+                         return result;
+               }
+
+               return true;
+          }
      }
 }
