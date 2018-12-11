617e614e6054d1682f01c4aae21f6177e672ac30
Yegor Stephanovich <stefanovich_egor@mail.ru>
2018-12-11 22:14:09 +0300

    add class dependency

---
+++ DependecyInjectionLibrary/Configuration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependecyInjectionLibrary
+{
+
+    public class Configuration
+    {
+          public List<Dependency> Dependencies { get; }
+
+          public Configuration()
+          {
+               Dependencies = new List<Dependency>();
+          }
+
+
+
+    }
+}
---
+++ DependecyInjectionLibrary/Dependency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependecyInjectionLibrary
+{
+    public class Dependency
+    {
+          public KeyValuePair<Type, Type> pair;
+          public bool isSingleton;
+
+          public Dependency(KeyValuePair<Type, Type> pair, bool isSinglton)
+          {
+               this.pair = pair;
+               this.isSingleton = isSingleton;
+          }
+    }
+}
